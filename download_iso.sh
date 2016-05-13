a255c63ea32e6eb2c57ba1ee8ca727b188005c3a
BuildTools <unconfigured@null.spigotmc.org>
2016-05-13 06:58:09 -0400

    Added correct download link

--- download_iso.sh
+++ download_iso.sh
@@ -1,3 +1,3 @@
-export PVNB_IMG="http://cdimage.debian.org/debian-cd/8.3.0/i386/iso-cd/debian-8.3.0-i386-netinst.iso"
+export PVNB_IMG="http://mirror.us.leaseweb.net/ubuntu-cdimage/xubuntu/releases/14.04.4/release/xubuntu-14.04-desktop-i386.iso"
 
 wget --show-progress --progress=bar:force $PVNB_IMG
